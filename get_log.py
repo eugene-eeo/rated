dab502c0d9a0c37f5c09ccd59f645a4bbb5ba41a
Eeo Jun <141bytes@gmail.com>
2019-02-17 11:50:47 +0000

    update

--- get_log.py
+++ get_log.py
@@ -2,7 +2,17 @@ import sys
 from Pyro4 import Proxy, locateNS
 from models import Update
 
+
+def d_sort(v):
+    # python 3: dicts preserve key insertion
+    # order so use this to our advantage
+    d = {}
+    for k in sorted(v.keys()):
+        d[k] = v[k]
+    return d
+
+
 r = Proxy(locateNS().lookup("replica:%s" % sys.argv[1])).get_log()[1]
 for u in r:
     u = Update(*u)
-    print(u.id, u.node_id, u.ts)
+    print(u.id, u.node_id, d_sort(u.ts))
