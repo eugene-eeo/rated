c4cda6f73374732b67a910d12c6065b161f3b8ca
Eeo Jun <141bytes@gmail.com>
2019-02-27 11:52:11 +0000

    remove

--- test_sort.py
+++
@@ -1,9 +0,0 @@
-from utils import sort_buffer, apply_updates
-from models import Update
-
-x = [
-    Update('a', 1, 1, 1, {}, {'x': 1}, 0),
-    Update('b', 1, 1, 1, {}, {'y': 1}, 0),
-    Update('c', 1, 1, 1, {"x": 1, 'y': 1}, {'y': 2}, 0),
-    Update('d', 1, 1, 1, {"x": 1, 'y': 1}, {'x': 2}, 0),
-]
